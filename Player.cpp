abd01764df80052fd1008128b9f8dea135c02593
rmit-s3699329-ZhiWei-Hou <42132514+rmit-s3699329-ZhiWei-Hou@users.noreply.github.com>
2019-09-30 10:00:32 +1000

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,67 @@
 #include "Player.h"
 
-Player::Player(std::string name) : name(_name), points(0) {
+Player::Player(std::string name){
+  tilesOnHand = new LinkedList();
+  tileToReplace = nullptr;
+  this->name = name;
+}
+
+Player::~Player(){
+  delete tilesOnHand;
+}
+
+void Player::setName(std::string name){
+  this->name = name;
+}
+
+void Player::addTiles(Tile* tile){
+  tilesOnHand->add(tile);
+}
+
+void Player::showTilesOnHand(){
+  Node* current = tilesOnHand->getHead();
+  std::cout << "Your hand is, " << std::endl;
+
+  while (current != nullptr){
+    if (current->next != nullptr)
+      std::cout << current->getTile()->toString() << ", ";
+    else
+      std::cout << current->getTile()->toString();
+    current = current->next;
+  }
+  std::cout << std::endl;
+}
+
+void Player::removeTile(std::string name){
+  if (tilesOnHand->has(name))
+    tilesOnHand->remove(name);
+}
+
+Tile* Player::findTile(std::string name){
+  Tile* tile = nullptr;
+
+  if (tilesOnHand->has(name))
+    tile = tilesOnHand->findNode(name)->getTile();
+  return tile;
+}
+
+void Player::addScore(int score){
+  this->score += score;
+}
+
+int Player::setScore(int score){
+  this->score = score;
+  return score;
+}
+
+LinkedList* Player::getTilesOnHand(){
+  return tilesOnHand;
+}
+
+std::string Player::getName(){
+  return name;
+}
+
+int Player::getScore(){
+  return score;
 }
--- Player.h
+++ Player.h
@@ -1,16 +1,41 @@
-#ifndef ASSIGN2_PLAYER_H
-#define ASSIGN2_PLAYER_H
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "TileCodes.h"
+#include <iostream>
+#include "LinkedList.h"
+#include "Tile.h"
 
 class Player {
 public:
-	Player(std::string _name);
-	std::string getName();
-	int getPoints();
-	
+  Player(std::string name);
+
+  ~Player();
+
+  void setName(std::string name);
+
+  void addTiles(Tile* tile);
+
+  void addScore(int score);
+
+  void removeTile(std::string name);
+
+  void showTilesOnHand();
+
+  LinkedList* getTilesOnHand();
+
+  Tile* findTile(std::string name);
+
+  std::string getName();
+
+  int getScore();
+
+  int setScore(int score);
 private:
-	std::string name;
-	int points;
-	
+  std::string name;
+  int score;
+  LinkedList* tilesOnHand;
+  Tile* tileToReplace;
 };
 
-#endif // ASSIGN2_NODE_H
+#endif
